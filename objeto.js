0d762635611d0b7b675f494bbf0efe0d3d67e919
airtonsm <airtonsmorais28@gmail.com>
2020-11-29 17:51:19 -0300

    Estudos js

---
+++ objeto.js
@@ -0,0 +1,15 @@
+const prod1 = {}
+prod1.nome = 'Celular ultra Mega'
+prod1.preco = 4998.90
+prod1['desconto legal'] = 0.40 //evetiar atributos com espaços
+
+console.log(prod1)
+
+const prod2 = {
+    nome: 'Camisa polo',
+    preco: 79.90
+}
+
+'{"nome": "Camisa polo","preco": 79.90 }'
+
+console.log(prod2)
